5dce11bae8050b0d669010c85472ec04897fded5
JaewoongMoon <mjw8585@gmail.com>
2016-12-20 00:27:23 -0500

    add ruby config 2

--- init.el
+++ init.el
@@ -362,4 +362,9 @@
 ;***************************************************************************/
 ;************************* For Ruby programming  ***************************/
 ;**************************************************************************/
+(add-to-list 'auto-mode-alist
+			 '("\\.\\(?:cap\\|gemspec\\|irbrc\\|gemrc\\|rake\\|rb\\|ru\\|thor\\)\\'" . ruby-mode))
+(add-to-list 'auto-mode-alist
+			 '("\\(?:Brewfile\\|Capfile\\|Gemfile\\(?:\\.[a-zA-Z0-9._-]+\\)?\\|[rR]akefile\\)\\'" . ruby-mode))
+
 
