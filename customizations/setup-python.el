190a18c5543ce4f64ed5c2ba2d1d94a9df057b20
JaewoongMoon <mjw8585@gmail.com>
2018-03-27 22:39:49 -0400

    fix python config

--- customizations/setup-python.el
+++ customizations/setup-python.el
@@ -10,7 +10,6 @@
              (setq index-level 4)
              (setq python-indent 4)
              (setq tab-width 4)
-			 (py-toggle-shell "python3") ; python3 를 기본 인터프리터로 설정
 			 ))
 
 ;; code-assist
@@ -21,6 +20,14 @@
 
 
 ; execution & debugging
+;; python3 를 기본 인터프리터로 설정
+(setq-default py-shell-name "python3")
+
+(add-hook 'python-mode-hook
+          '(lambda ()
+			 (py-toggle-shell "python3")
+			 ))
+
 ;; 버퍼 실행(C-c C-c) 시 화면분할은 양옆으로(horizontally) 나눠지도록
 (setq-default py-split-windows-on-execute-function 'split-window-horizontally)
 
